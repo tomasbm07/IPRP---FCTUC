c65b28e966eebeaffd3bfc91941a4c2230c857e8
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2019-12-04 15:25:22 +0000

    Corrected the Exercise

--- Outros Exercicios/Exercicios testes anteriores/2016-2017 Ex.2.py
+++ Outros Exercicios/Exercicios testes anteriores/2016-2017 Ex.2.py
@@ -1,27 +1,36 @@
-
 pos = {}
 string = 'x22ddd cbba'
 new_list = []
 
 for num, element in enumerate(string):
     pos[element] = pos.get(element, []) + [num]
-print(pos)
+# print(pos)
 
 
 def processa(string):
-    count = 0
+    a = 0
     elements = list(pos.keys())
     positions = list(pos.values())
-    reps = [0 for i in string]
+    reps = {}
     print(elements)
-    print(positions)
+    # print(positions)
 
-    for i in range(1, len(string)):
-        for j in range()
+    for x in range(len(string) - 1):
+        if string[x] == string[x+1]:
+            reps[string[x]] = reps.get(string[x], 0) + 1
+    print(reps)
 
+    keys = list(reps.keys())
+    print(keys)
 
     for i in range(len(elements)):
-        new_list.append((elements[i], positions[i][0], 0))
+        for j in range(len(keys)):
+            if elements[i] == keys[j]:
+                a = reps.get(keys[j])
+                break
+            else:
+                a = 0
+        new_list.append((elements[i], positions[i][0], a))
     print(new_list)
 
 
