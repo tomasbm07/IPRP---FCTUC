f3567aea698934e97d9706a123a4d3b9b1cb5026
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2019-12-28 16:37:38 +0000

    Uploaded

--- Outros Exercicios/Exame recurso 2018/2.py
+++ Outros Exercicios/Exame recurso 2018/2.py
@@ -1,6 +1,7 @@
 import turtle as t
 
 t.setworldcoordinates(-100, -100, 500, 500)
+t.speed(1)
 
 
 def small_line(comp):
@@ -19,17 +20,19 @@ def linha(tamanho, small_size, space_size):
     while flag == 1:
         lado += small_size
         if lado >= tamanho:
-            small_size = lado - tamanho
-            small_line(small_size)
-            flag = 0
+            extra = lado - tamanho
+            small_line(small_size - extra)
+            # flag = 0
+            break
         else:
             small_line(small_size)
 
         lado += space_size
         if lado >= tamanho:
-            space_size = lado - tamanho
-            small_space(space_size)
-            flag = 0
+            extra = lado - tamanho
+            small_space(space_size - extra)
+            # flag = 0
+            break
         else:
             small_space(space_size)
 
@@ -43,12 +46,12 @@ def retangulo(comprimento, largura, small_line, espaco):
 
 
 # Comprimento, Largura, tamanho do traçejado, espaço entre as linhas
-retangulo(200, 100, 5, 3)
+retangulo(300, 200, 15, 4)
 
 for i in range(2):
-    t.fd(200)
+    t.fd(300)
     t.lt(90)
-    t.fd(100)
+    t.fd(200)
     t.lt(90)
 
 t.hideturtle()
